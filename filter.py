f17026bc903b30d32fab37f498e9b9fb3a0784d2
dark7py <matveym20022002@gmail.com>
2021-11-18 18:33:45 +0500

    stage 1

--- filter.py
+++ filter.py
@@ -1,27 +1,28 @@
 from PIL import Image
 import numpy as np
+
 img = Image.open("img2.jpg")
 arr = np.array(img)
 a = len(arr)
 a1 = len(arr[1])
 i = 0
-while i < a - 11:
+while i <= a - 10:
     j = 0
-    while j < a1 - 11:
-        s = 0
+    while j <= a1 - 10:
+        pixels = 0
         for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                n1 = arr[n][n1][0]
-                n2 = arr[n][n1][1]
-                n3 = arr[n][n1][2]
-                M = n1 + n2 + n3
-                s += M
-        s = int(s // 100)
+            for k in range(j, j + 10):
+                r = arr[n][k][0]
+                g = arr[n][k][1]
+                b = arr[n][k][2]
+                M = (int(r) + int(g) + int(b)) / 3
+                pixels += M
+        pixels = int(pixels // 100)
         for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                arr[n][n1][0] = int(s // 50) * 50
-                arr[n][n1][1] = int(s // 50) * 50
-                arr[n][n1][2] = int(s // 50) * 50
+            for k in range(j, j + 10):
+                arr[n][k][0] = int(pixels // 50) * 50
+                arr[n][k][1] = int(pixels // 50) * 50
+                arr[n][k][2] = int(pixels // 50) * 50
         j = j + 10
     i = i + 10
 res = Image.fromarray(arr)
